bba7f64f8d1aba4e9cff26910cf8286ed050a689
Julien Rouse <julien.rouse@gmail.com>
2013-10-20 02:41:31 +0200

    demarrage des exos sur les listes, 4premiers sont fait

---
+++ list.pl
@@ -0,0 +1,21 @@
+% exercices about Lists
+
+%1) trouver le dernier element d''une liste 
+lastOfList(H,[H|[]]).
+lastOfList(X,[_|T]) :- lastOfList(X,T).
+
+ 
+%2) trouver l''avant dernier element d''une liste
+lastButOneOfList(H,[H,_|[]]).
+lastButOneOfList(X,[_|T]) :- lastButOneOfList(X,T).
+
+
+%3) trouver le k-ieme element d''une liste avec k en argument
+kthOfList(H,[H|_],1).
+kthOfList(X,[_|T],K) :-K1 is K-1, kthOfList(X,T,K1).
+
+
+%4) afficher le nombre d''element d''une liste
+nbrElementList(0,[]).
+nbrElementList(1,[_|[]]).
+nbrElementList(X,[_|T]) :-  nbrElementList(X1,T) , X is X1+1 .
