231ed2875367791ed84901b1cdd4952d06591685
Julien Rouse <julien.rouse@gmail.com>
2013-10-21 18:49:17 +0200

    ajout des ! partout, pour rendre les fonctions deterministes (je crois ^^)

--- list.pl
+++ list.pl
@@ -16,33 +16,34 @@ lastOfList(X,[_|T]) :- lastOfList(X,T).
 
  
 %2) trouver l''avant dernier element d''une liste
-lastButOneOfList(H,[H,_|[]]).
+lastButOneOfList(H,[H,_|[]]):- !.
 lastButOneOfList(X,[_|T]) :- lastButOneOfList(X,T).
 
 
 %3) trouver le k-ieme element d''une liste avec k en argument
-kthOfList(H,[H|_],1).
+kthOfList(H,[H|_],1):-!.
 kthOfList(X,[_|T],K) :-K1 is K-1, kthOfList(X,T,K1).
 
 
 %4) afficher le nombre d''element d''une liste
 nbrElementList(0,[]).
-nbrElementList(1,[_|[]]).
+nbrElementList(1,[_|[]]):-!.
 nbrElementList(X,[_|T]) :-  nbrElementList(X1,T) , X is X1+1 .
 
 
 %5 inverser une liste
-inverseList(X,X,[]).
+inverseList(X,X,[]):-!.
 inverseList(X,Y,[H2|T2]) :-  inverseList(X,[H2|Y],T2).
 
+inverseList([],[]):-!.
 inverseList(X,[H|T]) :- inverseList(X,[],[H|T]).
 
 
 %6 dire si la liste est un palindrome
 % solution elegante trouve sur http://stackoverflow.com/questions/8669685/prolog-palindrome-functor
 % beaucoup mieux que ce que j''avais fait
-isPalindrome([]).
-isPalindrome(X) :- inverseList(X,X).
+isPalindrome([]):- !.
+isPalindrome(X) :- !,inverseList(X,X).
 
 %7 Transform a list, possibly holding lists as elements into a 'flat' list by replacing each list with its elements (recursively).
 
